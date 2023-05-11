5f617989623e18e68e5ec5a8f84e9654de66091f
Filipe Bueno <filipe_bueno85@live.com>
2023-05-11 08:01:21 -0300

    Requisito 7 Crie o modelo `Category` em `src/models/Category.js` com as propriedades corretas

---
+++ src/models/Category.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: 'categories',
+    underscored: true,
+  });
+  return Category;
+}
